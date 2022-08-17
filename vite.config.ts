1d1f87e800fec9e2d482ef29112e5f1561c3b4ad
Iker Garcia <flipas.garcia@gmail.com>
2022-08-17 19:47:34 +0200

    chore(package): :sparkles: ➕ add eslint & formatter (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: './',
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  plugins: [react(), svgrPlugin()],
 })
