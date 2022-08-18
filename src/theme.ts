609a561ae8d0a843875a553de4ee280626ea55ff
Iker Garcia <flipas.garcia@gmail.com>
2022-08-18 23:11:40 +0200

    feat(theme): :lipstick: add material-ui theming

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+// A custom theme for this app
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+})
