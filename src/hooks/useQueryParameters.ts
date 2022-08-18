9d1e8b222bb3404c8f25fa25c7526b3a6e04760b
Iker Garcia <flipas.garcia@gmail.com>
2022-08-18 10:19:36 +0200

    feat(useQueryParameters): :sparkles: add hook to read query parameters

--- src/hooks/useQueryParameters.ts
+++ src/hooks/useQueryParameters.ts
@@ -1,13 +1,13 @@
-import { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useMemo } from 'react'
+import { useLocation } from 'react-router-dom'
 
-export default function useQueryParameters(){
-  const { search } = useLocation();
+export default function useQueryParameters() {
+  const { search } = useLocation()
   return useMemo(() => {
-    const params = new URLSearchParams(search);
+    const params = new URLSearchParams(search)
     return {
       owner: params.get('owner'),
       repository: params.get('repository'),
-    };
-  }, [search]);
+    }
+  }, [search])
 }
